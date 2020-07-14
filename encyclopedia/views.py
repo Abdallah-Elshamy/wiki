bcecad04cd7898c717cb3d24c6b3cdef6c2e1cb8
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2020-07-14 15:11:13 +0200

    Render entry pages

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render
 
 from . import util
 
+from markdown import markdown
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, entry_name):
+    entry = util.get_entry(entry_name);
+    if entry != None:
+        return render(request, "encyclopedia/entry.html",{
+            "entry": entry_name,
+            "content": markdown(entry)
+        })
+    
